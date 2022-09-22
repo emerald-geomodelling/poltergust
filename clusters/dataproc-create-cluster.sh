41673bbf3f7d4a26871f6661bde521d638c56e40
Egil <egil.moller@freecode.no>
2022-09-22 15:14:29 +0200

    Adde scripts for dataproc deployment

---
+++ clusters/dataproc-create-cluster.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+NAME="$1"
+echo $NAME
+[ -n "$NAME" ] || NAME=my-luigi-cluster
+NRWORKERS="$2"
+[ -n "$NRWORKERS" ] || NRWORKERS=3
+gsutil cp dataproc-initactions.sh "gs://${NAME}/initactions.sh"
+gcloud dataproc clusters create "$NAME" \
+  --region europe-north1 \
+  --zone europe-north1-a \
+  --master-machine-type n1-standard-4 \
+  --worker-machine-type n1-standard-4 \
+  --initialization-actions "gs://${NAME}/initactions.sh" \
+  --num-workers=$NRWORKERS \
+  --enable-component-gateway \
+  --master-boot-disk-size=40G \
+  --worker-boot-disk-size=40G \
+  --metadata dask-runtime=standalone \
+  --metadata dask-worker-on-master=false \
+  --metadata pipeline-url=gs://${NAME}/pipeline \
+  --image-version=2.0-ubuntu18

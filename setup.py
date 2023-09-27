bedd6c8c029ba95c06191c7c7da90df3a9ec5ae3
Egil <egil.moller@freecode.no>
2023-09-27 16:40:15 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='luigi-poltergust',
-    version='0.0.4',
+    version='0.0.5',
     description='Task trigger for Luigi',
     long_description='''Trigger Luigi tasks on multiple worker
 machines. Python modules for tasks and their dependencies are
@@ -23,6 +23,6 @@ installed automatically in virtualenvs on each worker.
         "requests",
         "google-auth",
         "google-api-python-client",
-        "poltergust-luigi-utils>=0.0.8"
+        "poltergust-luigi-utils>=0.0.12"
     ],
 )

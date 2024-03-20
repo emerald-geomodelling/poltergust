9d28cf8aebb392e94c46b80f364eae38362783fd
Egil <egil.moller@freecode.no>
2024-03-20 14:56:49 +0100

    Allow overriding hostname

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
+if [ "$HOSTNAME" == "" ]; then
+    HOSTNAME="$(hostname -f)"
+fi
+
 while true; do
-  luigi RunTasks --module poltergust --hostname="$(hostname -f)" --path="${PIPELINE_URL}" --scheduler-url="${SCHEDULER_URL}"
+  luigi RunTasks --module poltergust --hostname="${HOSTNAME}" --path="${PIPELINE_URL}" --scheduler-url="${SCHEDULER_URL}"
   sleep 1
 done

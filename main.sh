98da90ec59aa9f94cb798520cb97f37d069a37f1
MetalnAlloys <ali.scmenust@gmail.com>
2024-04-24 11:38:47 +0200

    run KillTask

--- main.sh
+++ main.sh
@@ -4,6 +4,12 @@ if [ "$HOSTNAME" == "" ]; then
     HOSTNAME="$(hostname -f)"
 fi
 
+#PIPELINE_URL="gs://experimental-pipeline-inversion-ali-v1/pipeline"
+#SCHEDULER_URL="http://localhost:8082"
+
+luigi KillTask --module poltergust --scheduler-url="${SCHEDULER_URL}" & 
+##>/dev/null &
+
 while true; do
   luigi RunTasks --module poltergust --hostname="${HOSTNAME}" --path="${PIPELINE_URL}" --scheduler-url="${SCHEDULER_URL}"
   sleep 1
